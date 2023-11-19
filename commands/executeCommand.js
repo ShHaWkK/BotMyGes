1666aaf1c9d53b2246505056f904d5a387ddb26f
Spatulox <M78stormtrooper@laposte.net>
2023-11-19 13:04:13 +0100

    reworked executeCommand.js

--- commands/executeCommand.js
+++ commands/executeCommand.js
@@ -1,30 +1,26 @@
-// Slashes command in alphabetical order...
+import { register } from "./commandsFunctions/register.js";
+import { setStatus } from "./commandsFunctions/setstatus.js";
 
-export async function executeSlashCommand(interaction){
+export async function executeSlashCommand(interaction, client){
     if (!interaction.isCommand()) return;
-  
-    if (interaction.commandName === 'blep') {
-        // Delete the command
-        await interaction.reply('Command under construction... :(');
-    }
 
     if (interaction.commandName === 'help') {
-        // Ajouter votre action ici
         await interaction.reply('help !');
     }
 
     if (interaction.commandName === 'register') {
-      // Ajouter votre action ici
-      await interaction.reply('Registered !');
+      await register(interaction)
     }
 
     if (interaction.commandName === 'rappel') {
-        // Ajouter votre action ici
         await interaction.reply('rappel !');
     }
 
+    if (interaction.commandName === 'set-status') {
+        await setStatus(interaction, client)
+    }
+
     if (interaction.commandName === 'set-up') {
-        // Ajouter votre action ici
         await interaction.reply('set-up !');
     }
       
