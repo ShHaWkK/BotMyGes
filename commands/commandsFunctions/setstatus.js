a8ebd016597daa3183b8154f0a55695947db33cf
Spatulox <M78stormtrooper@laposte.net>
2023-11-19 16:30:12 +0100

    reworked set-status command

--- commands/commandsFunctions/setstatus.js
+++ commands/commandsFunctions/setstatus.js
@@ -1,6 +1,15 @@
 import { log } from "../../functionnalities/globalFunct.js";
+import config from '../../config.json' assert { type: 'json' }
 
 export async function setStatus(interaction, client){
+
+    const owners = config.owners
+
+    if (!(owners.includes(interaction.user.username))){
+        interaction.reply('Your not the owner of the bot...')
+        return
+    }
+
     try{
         client.user.setActivity({
             name: interaction.options.getString('new-status')
